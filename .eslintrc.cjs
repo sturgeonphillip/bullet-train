b08e5838dcc45ceb1a23172cd812fc3a7c9e3fb8
Phillip Sturgeon <sturgeonphillip@protonmail.com>
2024-02-18 11:41:53 -0800

    eslint, prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,9 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    // 'prettier',
+    'prettier',
   ],
-  ignorePatterns: [ 'attic', 'dist', '.eslintrc.cjs'],
+  ignorePatterns: ['attic', 'dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
@@ -15,5 +15,56 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'array-bracket-spacing': 2,
   },
 };
+
+/**
+ * module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@stylistic/array-bracket-spacing/recommended', // Add this line to include the stylistic array-bracket-spacing rule
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'prettier',
+    '@stylistic/array-bracket-spacing', // Add this line to include the plugin
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    '@stylistic/array-bracket-spacing/array-bracket-spacing': [ // Add this rule
+      'error',
+      {
+        singleValue: true,
+        objectsInArrays: false,
+        arraysInArrays: false,
+        propertyValue: false,
+      },
+    ],
+  },
+};
+
+npm install --save-dev eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-jsx-a11y eslint-config-prettier eslint-plugin-prettier prettier eslint-plugin-array-bracket-spacing
+
+"scripts": {
+  "lint": "eslint .",
+  "lint:fix": "eslint --fix ."
+}
+
+ */
