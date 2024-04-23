bf9a1cc49dd233f376579c38bc5e6a921d9a5972
Phillip Sturgeon <sturgeonphillip@protonmail.com>
2024-04-23 16:37:54 -0700

    created findSurroundingLists (revised, better version of findAppropriateRoutineLists)

--- src/front/Massive/MainPage/mainPageFunctions.ts
+++ src/front/Massive/MainPage/mainPageFunctions.ts
@@ -10,6 +10,10 @@ export function isoDateKey() {
   return new Date(Date.now()).toISOString().split('T')[0];
 }
 
+export function fromUnix(unixTime: number) {
+  return new Date(unixTime).toISOString().split('T')[0];
+}
+
 export function createRoutine(name: string): RoutineProps {
   name = name ?? '[EMPTY]';
   return {
@@ -38,96 +42,90 @@ export async function fetchEntry(date: string) {
     }
 
     const data = await res.json();
-    console.log('DATA');
     return data;
   } catch (err) {
     // TODO: change error messaging
     console.error('Network response error.', err);
     return null;
   }
-
-  console.log('ROTATE');
 }
 
-// searches stored lists of routines to find the lists closest to
-// input date (before and after) when creating a missing entry
-export async function findAppropriateRoutineLists(
-  dateToMatch: string
-): Promise<ListOptionProps[]> {
-  const routineList: ListOptionProps[] = [];
-  const match = new Date(dateToMatch).getTime();
-
+export async function handleToday(date: string) {
   try {
-    const res = await fetch(`http://localhost:3001/list/`);
-    const json = await res.json();
-
-    const orderedDates = Object.keys(json)
-      .map((x) => new Date(x).getTime())
-      .sort((a, b) => a - b);
-
-    // const before = oldestBefore(orderedDates, match);
-    // const after = oldestAfter(orderedDates, match);
-
-    const before = oldestBefore(orderedDates, match);
-    const after = oldestAfter(orderedDates, match);
-
-    console.log('b', before);
-    console.log('a', after);
-    routineList.push(listWithKey(before, json));
-    if (before !== after) {
-      routineList.push(listWithKey(after, json));
+    const res = await fetch(`http://localhost:3001/entry/${date}`);
+    if (!res.ok) {
+      throw new Error('Network response error.');
     }
+
+    const data = await res.json();
+    console.log(data);
   } catch (err) {
-    console.error(err);
+    console.error('Neetwork response error.', err);
   }
-
-  return routineList;
 }
 
-// finds date of oldest list subsequent to entry date
-function oldestBefore(allLists: number[], dateMatch: number) {
-  return allLists.reduce((mostRecent, current) => {
-    if (current >= dateMatch && current < mostRecent) {
-      return current;
-    }
-    return mostRecent;
-  });
-}
-
-// finds the date of oldest list after entry date
-function oldestAfter(allLists: number[], dateMatch: number) {
-  return allLists.reduce((leastRecent, current) => {
-    if (current <= dateMatch && current > leastRecent) {
-      return current;
-    }
-    return leastRecent;
-  });
-}
-
-// format routine lists into tuple
+// format tuple [date, [routines]]
 function listWithKey(num: number, routineLists: ListProps): ListOptionProps {
   const key = new Date(num).toISOString().split('T')[0];
 
   return [key, routineLists[key]];
 }
 
+// const ordered = [
+//   1708473600000, 1708646400000, 1710547200000, 1711411200000, 1711929600000,
+// ];
+
+export async function findSurroundingLists(inputDate: string) {
+  const routineLists: ListOptionProps[] = [];
+  let storedLists: ListProps = {};
+
+  const midpoint = new Date(inputDate).getTime();
+
+  try {
+    const res = await fetch(`http://localhost:3001/list/`);
+    const json = await res.json();
+
+    storedLists = await json;
+  } catch (err) {
+    console.error(err);
+  }
+
+  const keys = Object.keys(storedLists);
+
+  const timecodes = keys.map((x) => new Date(x).getTime());
+
+  timecodes.push(midpoint);
+  timecodes.sort((a, b) => a - b);
+
+  const midIdx = timecodes.indexOf(midpoint);
+
+  const before = midIdx > 0 ? timecodes[midIdx - 1] : timecodes[midIdx];
+  const after =
+    midIdx < timecodes.length - 1 ? timecodes[midIdx + 1] : timecodes[midIdx];
+
+  // const beforeDate = new Date(before).toISOString().split('T')[0];
+
+  // const afterDate = new Date(after).toISOString().split('T')[0];
+
+  routineLists.push(listWithKey(before, storedLists));
+  routineLists.push(listWithKey(after, storedLists));
+
+  console.log(
+    'rL',
+    routineLists,
+    midIdx,
+    // beforeDate,
+    '|',
+    // afterDate,
+    '|',
+    'sort timecodes',
+    timecodes
+    // timecodes.sort((a, b) => a - b)
+  );
+}
+
 (async () => {
-  // const res = await fetchEntry('2024-02-29');
-  // console.log('res', res);
-
-  const res = await findAppropriateRoutineLists('2024-03-18');
-  console.log('res', res);
+  console.log('findRoutineLists...');
+  const r = await findSurroundingLists('2024-03-17');
+  console.log(r);
 })();
-/**
- * // (async () => {
-//   const res = await findAppropriateRoutineList('2024-02-29');
-//   console.log('res', res);
-// })();
- */
-
-// const b = 1710547200000;
-// const a = 1708473600000;
-// console.log(b - a);
-// console.log(a - b);
-// console.log('before', new Date(b));
-// console.log('after', new Date(a));
