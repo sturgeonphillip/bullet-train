e419c17ca0c26667db2b96fa9ab9b2050cfffb32
Phillip Sturgeon <sturgeonphillip@protonmail.com>
2024-05-29 09:52:12 -0700

    active components viewed for dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import './App.css';
 import { Clock } from './front/Clock/Clock';
-// import Errands from './front/Errands/Display';
+import Errands from './front/Errands/Display';
 // import Main from './front/Intended/Fix/Main';
-import List from './front/Lists/Display';
-import { History } from './front/Lists/HistoryTable-v2';
+// import List from './front/Lists/Display';
+
+// import { HistoryColumn } from './front/Lists/HistoryColumn/HistoryColumn';
+// import { HistoryRow } from './front/Lists/HistoryRow/HistoryRow';
 import { Loader } from './front/Loader/Loader';
 
 function App() {
   return (
     <>
-      {/* <Main /> */}
-      <hr />
       <Clock />
-      {/* <Errands /> */}
-      <hr />
-      <Loader />
-      <hr />
-      <History />
-      <hr />
+      {/* <Main /> */}
+      {/* 
+      // {/* <Errands /> */}
+      {/* <hr /> */}
+      {/* <HistoryColumn /> */}
+      {/* <HistoryRow /> */}
 
-      <List />
+      {/* <List /> */}
+      <Loader />
+      <Errands />
+      <hr />
     </>
   );
 }
